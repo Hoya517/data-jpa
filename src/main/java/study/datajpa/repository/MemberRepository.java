d9c7e41082fcf1f26326f5965e7ff8df3942c5c3
Kim Juho <sv05159@naver.com>
2021-11-16 00:07:36 +0900

    스프링 데이터 JPA 페이징과 정렬
    Top, First 사용 참고 https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.limit- query-result

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -40,6 +40,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findMemberByUsername(String username);  //단건
     Optional<Member> findOptionalByUsername(String username);  //단건 Optional
 
-    @Query(value = "select m from Member m")
-    Page<Member> findByAge(int age, Pageable pageable);
+    Page<Member> findTop3ByAge(int age, Pageable pageable);
 }
