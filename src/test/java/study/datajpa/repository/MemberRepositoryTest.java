44e2527c9aac34505306f924b872c912a7a1e5fc
Kim Juho <sv05159@naver.com>
2021-11-15 23:58:37 +0900

    스프링 데이터 JPA 페이징과 정렬
    Slice 테스트
    - count 쿼리 없음.
    - limit는 3개 요청했는데 4개 걸림. (한개 더 가져와서 더 있으면 +더보기)

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -191,15 +192,15 @@ class MemberRepositoryTest {
         PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
 
         //when
-        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+        Slice<Member> page = memberRepository.findByAge(age, pageRequest);
 
         //then
         List<Member> content = page.getContent();           //조회된 데이터
 
         assertThat(content.size()).isEqualTo(3);            //조회된 데이터 수
-        assertThat(page.getTotalElements()).isEqualTo(5);   //전체 데이터 수
+//        assertThat(page.getTotalElements()).isEqualTo(5);   //전체 데이터 수
         assertThat(page.getNumber()).isEqualTo(0);          //페이지 번호
-        assertThat(page.getTotalPages()).isEqualTo(2);      //전체 페이지 번호
+//        assertThat(page.getTotalPages()).isEqualTo(2);      //전체 페이지 번호
         assertThat(page.isFirst()).isTrue();                //첫번째 항목인가?
         assertThat(page.hasNext()).isTrue();                //다음 페이지가 있는가?
     }
